9b5b5036b29f4985076e04d4928f5c959f1604d4
Luis <estevezl@mail.desy.de>
2018-06-08 00:50:05 +0200

    New histograms added:    -pT_allBHad_vs_pT-bjets    -pT_leadBHad_vs_pT-bjets    -BHad_Multiplicity_vs_BHad_pT    -Delta phi distribution for jets    -Delta phi distribution for b-jets

--- CMS_2012_S9383875.cc
+++ CMS_2012_S9383875.cc
@@ -40,7 +40,7 @@ namespace Rivet {
         /// @todo Initialise and register projections here
             const FinalState cnfs(-4, 4);
             addProjection(cnfs, "FS");
-            addProjection(FastJets(cnfs, FastJets::ANTIKT, 0.5), "Jets");
+            addProjection(FastJets(cnfs, FastJets::ANTIKT, 0.4), "Jets");
 
             IdentifiedFinalState mufs(cnfs);
             mufs.acceptId(PID::MUON);
@@ -61,7 +61,7 @@ namespace Rivet {
 		      		
             _h_p1d        = bookProfile1D("NBHad_vs_b-jet-pT",60,15,2000);	   
 
-            vector<double> Ptbining {15, 18, 21, 24, 28, 32, 37, 43, 49, 56, 64, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468,507, 548, 592, 638, 686, 737, 790, 846, 905, 967,1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1684, 1784, 1890, 2000 }; 
+            vector<double> Ptbining {0,3,6,9,12,15, 18, 21, 24, 28, 32, 37, 43, 49, 56, 64, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468,507, 548, 592, 638, 686, 737, 790, 846, 905, 967,1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1684, 1784, 1890, 2000 }; 
 
 //,2116, 2238, 2366, 2500, 2640, 2787, 2941, 3103, 3273, 3450, 3637, 3832,4037, 4252, 4477, 4713, 4961, 5220, 5492, 5777, 6076, 6389, 6717, 7000};
 
@@ -71,9 +71,12 @@ namespace Rivet {
                 NBHadBinning.push_back(i-0.5);
 
             pt_N = bookHisto2D("BHad_mult_vs_b-jet-pT", Ptbining, NBHadBinning,"BHad_mult_vs_b-jet-pT","pt","N");
-            ptbj_ptBHad = bookHisto2D("pT_b-jet_vs_pT-BHad",Ptbinig, Ptbning,"pT_b-jet_vs_pT-BHad","b-jet_pT","BHad_pT");          
+          
+            ptbj_ptBHad = bookHisto2D("pT_b-jet_vs_pT-BHad",Ptbining, Ptbining,"pT_b-jet_vs_pT-BHad","b-jet_pT","BHad_pT");          
+            ptbj_ptallBHad =  bookHisto2D("pT_b-jet_vs_pT-allBHad",Ptbining, Ptbining,"pT_b-jet_vs_pT-BHad","b-jet_pT","BHad_pT"); 
+          
             _h_dsigdphi = bookHisto1D("dsigmadphi_jets",50,0,M_PI,"dsigma/dphi for jets", "phi","dsigma/dphi");
-		      _h_dsigdphi_bjets = bookHisto1D("dsigmadphi_bjets",40,0,M_PI,"dsigma/dphi for b-jets", "phi","dsigma/dphi");                        
+		      _h_dsigdphi_bjets = bookHisto1D("dsigmadphi_bjets",50,0,M_PI,"dsigma/dphi for b-jets", "phi","dsigma/dphi");                        
                       
             Bjet18 = 0.;
             Bjet32 = 0.;
@@ -81,6 +84,39 @@ namespace Rivet {
                      
             }
                 
+
+
+           //function which gives nB and maximal pT of bHadrons close to jet j
+           void getBmaxPtAndNBs(const Jet &j, const Particles& bHadrons, int &nB, double &pT_Max) {
+              pT_Max = 0;
+              nB = 0;
+              foreach(const Particle& b, bHadrons) { 
+                 double difference = deltaR(j.momentum(), b.momentum());
+                 if(difference < 0.4) {
+                    pT_Max = max(pT_Max, b.momentum().pT());
+                    ++nB;
+                 }
+              }
+           }
+
+           double getBmaxPt(const Jet &j, const Particles& bHadrons) {
+              int nB;
+              double pT_Max;
+              getBmaxPtAndNBs(j, bHadrons, nB,pT_Max);
+              return pT_Max;
+           }
+
+
+           int nBs(const Jet &j, const Particles& bHadrons) {
+              int nB;
+              double pT_Max;
+              getBmaxPtAndNBs(j, bHadrons, nB,pT_Max);
+              return nB;
+           }
+
+
+
+
                  
       /// Perform the per-event analysis
       void analyze(const Event& event) {
@@ -97,98 +133,50 @@ namespace Rivet {
                      
 
              bool muongoodevent=false;
-                      
-             bool onebtag=false;
+             bool onebtag      =false;
             
-             //For b-jets 
-             double deltaphi = 0.; 
-             double leadj_phi = 0.;
-	          double subleadj_phi = 0.;
-             
-             //For jets
-             double deltaphi_jets = 0.;
-             double leadj_phi_jet = 0.;
-             double subleadj_phi_jet = 0.;
-
-             unsigned int num_jets = 0;            
-             unsigned int num_b_jets = 0;                       
-           
              foreach (const Jet& j, jets) {
-                               
-                  bool btag=false;
-                  int BHad =0;
-			    	   ++num_jets;
+
+                  //number of Bhadrons close to jet j
+                  int BHad = nBs(j, bHadrons); 
                   
-                  if (num_jets < 3){
-
-                        if (num_jets == 1)
-                        leadj_phi_jet = j.momentum().phi();
-                        else if(num_jets == 2)
-                        subleadj_phi_jet = j.momentum().phi();
-
-                        deltaphi_jets = deltaPhi(leadj_phi_jet,subleadj_phi_jet);
-                       // cout<<"********************* num_jets ="<<num_jets<<" ******************************"<<endl;
-                       // cout<<"deltaphi_jets= "<<deltaphi_jets<<endl;
-                  }
-
-                  const double pT_BHad; 
-                  bool BHad_lead = true;
-                 
-                  foreach(const Particle& b, bHadrons) { 
-
-                         double difference = deltaR(j.momentum(), b.momentum());
-                                 
-                          if(  difference <0.4) {
-                               btag=true;
-                               onebtag=true;  
-				                   ++BHad;
-                               if(BHad_lead){
-                                  pT_BHad = b.momentum().pT();
-                                  BHad_lead = false; 
-			          cout << "*****************  PT_BHad = "<< pT_BHad <<"***********************"<<endl;		
-                               }
-                           }
-                  }
-
-				
-                           
-                  if(btag){
-
-                        num_b_jets += 1;
-                               
-			               if (num_b_jets < 3){  	
-                           
-                              if (num_b_jets == 1) 
-				                  leadj_phi = j.momentum().phi();
-                              else if(num_b_jets == 2)
-				                  subleadj_phi = j.momentum().phi();
-                                  
-			                     deltaphi = deltaPhi(leadj_phi,subleadj_phi);
-                             // cout<<"*************** "<<"num_b_jets =" <<num_b_jets <<" *****************************"<<endl;
-                             // cout <<"deltaphi_bjets ="<< deltaphi<<endl;   
-                        }
-
-                        const double ptB = j.momentum().pT();
-                        const double yB = j.momentum().rapidity();
+                  //filling the BHad_pT of all BHad vs b-jet pT
+                 foreach(const Particle& b, bHadrons) { 
+                     double difference = deltaR(j.momentum(), b.momentum());
+                     if(difference < 0.4) {
+                        ptbj_ptallBHad -> fill(j.momentum().pT(),b.momentum().pT(),weight);       
                         
-                         if( fabs(yB) < 0.5) _h_dsigdpty05->fill( ptB, weight );
-                         else if( fabs(yB) > 0.5 && fabs(yB) < 1.0) _h_dsigdpty10->fill( ptB, weight );
-                         else if( fabs(yB) > 1.0 && fabs(yB) < 1.5) _h_dsigdpty15->fill( ptB, weight );
-                         else if( fabs(yB) > 1.5 && fabs(yB) < 2.0) _h_dsigdpty20->fill( ptB, weight );
-                         else if( fabs(yB) > 2.0 && fabs(yB) < 2.2) _h_dsigdpty22->fill( ptB, weight );
-                         if ( fabs(yB) < 2.2 && ptB > 18. ) Bjet18 = Bjet18 + weight;
-                         if ( fabs(yB) < 2.2 && ptB > 32. ) Bjet32 = Bjet32 + weight;
-                         
-                        // cout<<"num_b-jets = "<< num_b_jets<<endl;
-                        // cout<<"num_b-hadrons ="<< BHad << endl;
+                       //cout <<"pT_jet = " << j.momentum().pT() <<endl;
+                       //cout <<"pT_BHad = "<< b.momentum().pT() << endl;
 
-                         pt_N->fill(ptB,BHad,weight);
-                        _h_p1d -> fill(ptB,BHad, weight);
-                         ptbj_ptBHad -> fill(ptB, pT_BHad, weight);            
+                     }
+                 }
 
+                  if(BHad > 0) { //is bJet
+                        onebtag = true;
+                        const double ptB = j.momentum().pT();
+                        const double yBa = abs(j.momentum().rapidity());
+                        
+                         if( yBa < 0.5)                   _h_dsigdpty05->fill( ptB, weight );
+                         else if( yBa > 0.5 && yBa < 1.0) _h_dsigdpty10->fill( ptB, weight );
+                         else if( yBa > 1.0 && yBa < 1.5) _h_dsigdpty15->fill( ptB, weight );
+                         else if( yBa > 1.5 && yBa < 2.0) _h_dsigdpty20->fill( ptB, weight );
+                         else if( yBa > 2.0 && yBa < 2.2) _h_dsigdpty22->fill( ptB, weight );
+
+                         if ( yBa < 2.2 && ptB > 18. ) Bjet18 +=  weight;
+                         if ( yBa < 2.2 && ptB > 32. ) Bjet32 +=  weight;
+
+                         double pT_BHad_Max = getBmaxPt(j, bHadrons);
+                        //cout << "pT_BHad_Max = "<< pT_BHad_Max <<endl;  
+
+                         pt_N ->fill(ptB, BHad,weight);
+                        _h_p1d->fill(ptB, BHad, weight);
+                         ptbj_ptBHad -> fill(ptB, pT_BHad_Max, weight);            
+                        
                    }
 
 
+                  //Analysis with muons
                    if(j.momentum().pT()>30 && fabs(j.momentum().rapidity())<2.4){
 
                         foreach (const Particle& muon, muons.particles()) {
@@ -206,10 +194,15 @@ namespace Rivet {
                    }
             }
            
-            if( num_b_jets > 2) 
-            _h_dsigdphi_bjets -> fill(deltaphi,weight);
-            if( num_jets > 2)
-            _h_dsigdphi -> fill(deltaphi_jets,weight);
+            //Delta Phi for jets above 100 GeV
+            if(jets.size() >= 2 && jets[1].pt() > 100*GeV) {
+               double deltaphi = deltaPhi(jets[0].phi(),jets[1].phi());
+               _h_dsigdphi -> fill(deltaphi, weight);
+               if(nBs(jets[0], bHadrons) && nBs(jets[1], bHadrons)) {
+                     _h_dsigdphi_bjets -> fill(deltaphi, weight);
+               }
+            }
+
       }
                    
        /// Normalise histograms etc., after the run
@@ -238,7 +231,7 @@ namespace Rivet {
             scale(_h_dsigdpt, invlumiNano);
             //scale(_h_dsigdy, invlumiNano/0.5);
             scale(_h_dsigdy, invlumiNano);
-
+          
       }
 
       //@}
@@ -262,7 +255,8 @@ namespace Rivet {
       Profile1DPtr _h_p1d;
       Histo2DPtr pt_N;							      
       Histo2DPtr ptbj_ptBHad;
-      
+      Histo2DPtr ptbj_ptallBHad; 
+
       Histo1DPtr _h_dsigdphi_bjets;
       Histo1DPtr _h_dsigdphi;
 
